b1986c90c01ef9cc56576017471cda6fba8eb850
alinooralian <144942445+alinooralian@users.noreply.github.com>
2026-01-03 00:17:41 +0330

    module nevisi

--- landing.py
+++ landing.py
@@ -14,13 +14,91 @@ with open("users.json", "r") as f:
 
 players = dict()
 
+def first_menu():
+    print("1.New Game")
+    print("2.Load Game")
+    print("3.Leaderboard")
+    print("4.Exit")
+    key = input()
+    
+    return key
+
+def new_game_menu():
+    while True:
+        game_name = input("Enter your game name: ") + ".json"
+        path = Path(__file__).parent / "old_games" / game_name
+        if path.exists():
+            print("This game already exists.")
+        else:
+            with open(path, "w", encoding="utf-8") as f:
+                pass
+            
+            with open(path, "r", encoding="utf-8") as f:
+                try:
+                    players = json.load(f)
+                except json.JSONDecodeError:
+                    players = {}
+            break
+    
+    i = 1
+    while True:
+        if len(players) >= 4:
+            break
+        print("1.Singup")
+        print("2.Login")
+        print("3.Exit")
+        key = input()
+        
+        if key == '1':
+            while True:
+                if signup(path):
+                    break
+        
+        if key == '2':
+            while True:
+                if login(path, 1):
+                    break
+        if key == '3':
+            break
+    
+    return key
+
+def load_game_menu():
+    game_name = input("Enter your game name: ") + ".json"
+    path = Path(__file__).parent / "old_games" / game_name
+
+    with open(path, "r", encoding="utf-8") as f:
+        try:
+            players = json.load(f)
+        except json.JSONDecodeError:
+            players = {}
+
+    if len(players) < 4:
+        print(f"You have fewer than 4 players. {4 - len(players)} more people must register first.")
+        cnt = 4 - len(players)
+        while cnt:
+            while True:
+                if signup(path):
+                    break
+            cnt -= 1
+        
+        print("Now you can log in to your account to continue playing.")
+
+    cnt = 1
+    while cnt <= 4:
+        while True:
+            if login(path, 2):
+                break
+        
+        cnt += 1
+
 def hash_password(password):
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 def check_password(input_password, stored_hash):
     return bcrypt.checkpw(input_password.encode("utf-8"), stored_hash.encode("utf-8"))
 
-def signup():
+def signup(path):
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     email = input("Enter your email: ")
@@ -57,9 +135,11 @@ def signup():
         json.dump(users, f, ensure_ascii=False, indent=4)
     with open(path, "w", encoding="utf-8") as f:
         json.dump(players, f, ensure_ascii=False, indent=4)
+    
+    print(f"Hello {username}! You are ready to play.")
     return True
 
-def login(Type):
+def login(path, Type):
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     
@@ -95,79 +175,22 @@ def login(Type):
     else:
         print("Username not found.")
     
+    print(f"Hello {username}! You are ready to play.")
     return False
 
 
-print("1.New Game")
-print("2.Load Game")
-print("3.Leaderboard")
-print("4.Exit")
-key = input()
+key = first_menu()
 
 if key == '1':
-    while True:
-        game_name = input("Enter your game name: ") + ".json"
-        path = Path(__file__).parent / "old_games" / game_name
-        if path.exists():
-            print("This game already exists.")
-        else:
-            with open(path, "w", encoding="utf-8") as f:
-                pass
-            
-            with open(path, "r", encoding="utf-8") as f:
-                try:
-                    players = json.load(f)
-                except json.JSONDecodeError:
-                    players = {}
-            break
-    
-    while True:
-        if len(players) >= 4:
-            break
-        print("1.Singup")
-        print("2.Login")
-        print("3.Exit")
-        key = input()
-        
+    key = new_game_menu()
+    while key == '3':
+        key = first_menu()
         if key == '1':
-            while True:
-                if signup():
-                    break
-        
-        if key == '2':
-            while True:
-                if login(1):
-                    break
-        
-        if key == '3':
-            exit()
-
+            key = new_game_menu_menu()
+        elif key == '2':
+            load_game_menu()
+            break
 elif key == '2':
-    game_name = input("Enter your game name: ") + ".json"
-    path = Path(__file__).parent / "old_games" / game_name
-
-    with open(path, "r", encoding="utf-8") as f:
-        try:
-            players = json.load(f)
-        except json.JSONDecodeError:
-            players = {}
-
-    if len(players) < 4:
-        print(f"You have fewer than 4 players. {4 - len(players)} more people must register first.")
-
-        while len(players) < 4:
-            while True:
-                if signup():
-                    break
-        
-        print("Now you can log in to your account to continue playing.")
-
-    cnt = 0
-    while cnt < 4:
-        while True:
-            if login(2):
-                break
-        
-        cnt += 1
+    load_game_menu()
 elif key == '4':
     exit()
